af5fd3c4e5d5355e36865367fb95edad8bee7fa2
zhangxizhe <15225027300@163.com>
2019-08-20 19:44:16 +0800

    zxz1

---
+++ src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import Vant from 'vant';
+import App from './App'
+import router from './router'
+import store from './store'
+import 'vant/lib/index.css';
+import axios from 'axios';
+import Vuex from 'vuex'
+import vueTap from 'v-tap';
+
+Vue.config.productionTip = false;
+Vue.prototype.$http = axios;
+Vue.use(Vant);
+Vue.use(Vuex);
+Vue.use(vueTap);
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
